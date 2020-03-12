e5edf17e7d0dbac1f10783054b78c032cbf2b821
Avgilles <gillouavraam@gmail.com>
2020-03-12 17:31:38 +0100

    Ajout de la liaison du serveur mongoo2

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
+
 const express= require('express');
+const mongoose= require('mongoose');
 
 const app = express();
 const blogRouter = require('./blog.router');
@@ -15,7 +18,30 @@ const HOST = 'localhost';
 
 app.use('/', blogRouter);
 
-app.listen(PORT,HOST, ()=>{
-    console.log(`Express: Le serveur écoute sur http://${HOST}:${PORT}`)
+// il faut placer les valeurs suivante dqns la chaine
 
-});
+// process.env.DB_USER;
+// process.env.DB_PASSWORD;
+// process.env.DB_HOST;
+// process.env.DB_NAME;
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
+
+mongoose.connect
+    (`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`, options)
+    .then(()=> console.log(`Moogose : Connexion établie à Atlas`))
+// je rajoute un then pour attendre qu'on se connecte à atlas puis express
+    .then(()=>{
+        app.listen(PORT,HOST, ()=>{
+            console.log(`Express: Le serveur écoute sur http://${HOST}:${PORT}`)
+        })
+    })
+
+    .catch((err)=> console.error(err));
+
+
+
+;
